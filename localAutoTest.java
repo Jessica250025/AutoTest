2f68711d6146153c5f7b85d9345e0e294dc60de4
Jessica250025 <summer.clementine@gmail.com>
2015-11-30 12:05:07 -0800

    Update localAutoTest.java

--- localAutoTest.java
+++ localAutoTest.java
@@ -5,12 +5,16 @@ import com.thoughtworks.selenium.*;
 import java.util.regex.Pattern;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class firstTest {
+public class localAutoTest {
 	 
 	 public static void main(String[] args) throws Exception {
 	 	 
@@ -21,20 +25,25 @@ public class firstTest {
 		  
 		//Launch the Paypal Website
 		driver.get("https://www.paypal.com/");
+		
 		// print a log of page title
 		String Title = driver.getTitle();
-		System.out.println("Successfully opened" + Title);
-		
-		Thread.sleep(500);  
+		System.out.println("Successfully opened: " + Title);
 		  
-			// Go to log in page
-		WebElement logInElem = driver.findElement(By.id("ul-btn"));
-		logInElem.click();
-		
-		// Type in email
-		WebElement element = driver.findElement(By.name("login_email"));  
+		// Go to log in page
+		driver.findElement(By.id("ul-btn")).click();
 		System.out.println("access login page...");
+	    ExpectedCondition<Boolean> pageLoadCondition = new
+	            ExpectedCondition<Boolean>() {
+	                public Boolean apply(WebDriver driver) {
+	                    return ((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete");
+	                }
+	            };
+	    WebDriverWait wait = new WebDriverWait(driver, 30);
+	    wait.until(pageLoadCondition);
 		
+	    // Type in email
+		WebElement element = driver.findElement(By.name("login_email"));  
 		element.sendKeys("ABCD_abcd");  
 		System.out.println("input user name...");  
 		
@@ -44,11 +53,15 @@ public class firstTest {
 		System.out.println("input password");  
 		
 		// login
-		Thread.sleep(500); 
 		driver.findElement(By.id("btnLogin")).click();
 		System.out.println("Loging in...");
-		  
-		Thread.sleep(500);  // Let the user actually see something!
+ 
+        // Wait for notifications to be present
+        element = wait.until(
+        	    ExpectedConditions.visibilityOfElementLocated(By.id("notifications")));
+		String alertText = element.getText();
+		System.out.println("Notification text: " + alertText);
+		
 		driver.quit();
 	
 	 }
